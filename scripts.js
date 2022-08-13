371a1cc0b1f8c23cf3be21523d4e981b6a989725
mit730 <107538978+mit730@users.noreply.github.com>
2022-08-13 15:21:16 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,107 +1,111 @@
-function swap(){
-    let inBase = document.getElementById('in-base');
-    let outBase = document.getElementById('out-base');
+function swap() {
+    let inputBase = document.getElementById('input-base');
+    let outputBase = document.getElementById('output-base');
 
-    swapStart(inBase, outBase);
+    swapStart(inputBase, outputBase);
+    
 
-    //swapping the values
-    let temp = inBase.value;
-    inBase.value = outBase.value;
-    outBase.value = temp;
+    // swap the values
+    let temp = inputBase.value;
+    inputBase.value = outputBase.value;
+    outputBase.value = temp;
 }
 
-// making swapping possible
-
-function swapStart(inBase, outBase) {
-    inBase.style.transform = 'translatex(70%)'
-    outBase.style.transform = 'translatex(-70%)'
-
+//for swap animation
+function swapStart(inputBase, outputBase) {
+    inputBase.style.transform = 'translateX(70%)'
+    outputBase.style.transform = 'translateX(-70%)'
     setTimeout(() => {
-        swapEnd(inBase, outBase);
+        swapEnd(inputBase, outputBase);
     }, 100);
 }
 
-function swapEnd(inBase, outBase) {
-    inBase.style.transform = 'translatex(0%)'
-    outBase.style.transform = 'translatex(0%)'
+function swapEnd(inputBase, outputBase) {
+    inputBase.style.transform = 'translateX(0%)'
+    outputBase.style.transform = 'translateX(0%)'
 }
 
-function convert(){
-    let txtNumber = document.getElementById('txtNumber').value;
+function convert() {
+
+    let input = document.getElementById('input').value;
     let output = document.getElementById('output');
-    let inBase = document.getElementById('in-base').value;
-    let outBase = document.getElementById('out-base').value;
+    let inputBase = document.getElementById('input-base').value;
+    let outputBase = document.getElementById('output-base').value;
     const error = document.getElementById('error');
-    txtNumber = txtNumber.toUpperCase();
+    input = input.toUpperCase();
+    
+    if(checkInput(input, inputBase, error)) {
 
-    if(checkInput(txtNumber, inBase, error)) {
         error.style.display = 'none';
-         //convert all input to decimal
-        txtNumber = Number.parseInt(txtNumber, inBase)
-         //then convert decimal to desired output
-         let result = Number(txtNumber).toString(outBase);
+        //convert all input to decimal
+        input = Number.parseInt(input, inputBase);  
+        //then convert decimal to desired output
+        let result = Number(input).toString(outputBase);
 
-         //displaying output
-         if( result != 'NaN') {
-            output.style.backgroundColor = 'white';
+        //display output
+        if(result != 'NaN') {
+            output.style.backgroundColor = 'rgb(211, 220, 122)';
             output.textContent = result.toUpperCase();
-         }else{
-            output.style.background = 'white';
+            
+        } else {
+            output.style.backgroundColor = 'white';
             output.textContent = ""
-         }
+        }
     }
 }
 
-function checkInput(txtNumber, inBase, error) {
-     //check if input number contains valid values
+function checkInput(input, inputBase, error) {
+    //check if input number contains allowed values
+    //for e.g octal number does not have number '8' and similarly binary number does not have number '3'
 
-     for( let i = 0; i < txtNumber.length; i++) {
-        if(txtNumber.charAt(i) == '.'){
+    for(let i = 0; i < input.length; i++) {
+        if(input.charAt(i) == '.') {
             error.style.display = 'block';
-            error.textContent = "Oops!! its an error"
+            error.textContent = "Error : I am extremely sorry. Given input number contains '.' and this page can only solve for numbers without decimal point."
             return false;
         }
-     }
-
-     switch (inBase) {
-        case '2':
-            for(let i = 0; i < txtNumber.length; i++) {
-                if(txtNumber.charAt(i) != 0 && txtNumber.charAt(i) != 1) {
-                    error.style.display = 'block';
-                    error.textContent = `Error : Binary number does not include '${txtNumber.charAt(i)}'. Please enter only binary number or select different base for input number`;
-                    return false;
-                }
-            }
-            case '10':
-            for(let i = 0; i < txtNumber.length; i++) {
-                if(isNaN(txtNumber.charAt(i))) {
-                    error.style.display = 'block';
-                    error.textContent = `Error : Decimal number does not include '${txtNumber.charAt(i)}'. Please enter only decimal number or select different base for input number`;
-                    return false;
-                }
-            }
-
-            case '8':
-                for(let i = 0; i < txtNumber.length; i++) {
-                    if(txtNumber.charAt(i) == '8' || txtNumber.charAt(i) == '9' || isNaN(txtNumber.charAt(i))) {
-                        error.style.display = 'block';
-                        error.textContent = `Error : Octal number does not include '${txtNumber.charAt(i)}'. Please enter only octal number or select different base for input number`;
-                        return false;
-                    }
-                }
-
-            case '16':
-                for(let i = 0; i < txtNumber.length; i++) {
-                if(!((txtNumber.charCodeAt(i) >= 48 
-                && txtNumber.charCodeAt(i) <= 57) || (txtNumber.charCodeAt(i) >= 65 
-                && txtNumber.charCodeAt(i) <= 70))) {
-                    error.style.display = 'block';
-                    error.textContent = `Error : Hexadecimal number does not include '${txtNumber.charAt(i)}'. Please enter only hexadecimal number or select different base for input number`;
-                    return false;
-                }
-            }
-
-            default:
-                return true;
-        }
     }
+
+    switch (inputBase) {
+        case '2':
+            for(let i = 0; i < input.length; i++) {
+                if(input.charAt(i) != 0 && input.charAt(i) != 1) {
+                    error.style.display = 'block';
+                    error.textContent = `Error : Binary number does not include '${input.charAt(i)}'. Please enter only binary number or select different base for input number`;
+                    return false;
+                }
+            }
+        
+        case '10':
+            for(let i = 0; i < input.length; i++) {
+                if(isNaN(input.charAt(i))) {
+                    error.style.display = 'block';
+                    error.textContent = `Error : Decimal number does not include '${input.charAt(i)}'. Please enter only decimal number or select different base for input number`;
+                    return false;
+                }
+            }
+
+        case '8':
+            for(let i = 0; i < input.length; i++) {
+                if(input.charAt(i) == '8' || input.charAt(i) == '9' || isNaN(input.charAt(i))) {
+                    error.style.display = 'block';
+                    error.textContent = `Error : Octal number does not include '${input.charAt(i)}'. Please enter only octal number or select different base for input number`;
+                    return false;
+                }
+            }
+
+        case '16':
+            for(let i = 0; i < input.length; i++) {
+                if(!((input.charCodeAt(i) >= 48 
+                && input.charCodeAt(i) <= 57) || (input.charCodeAt(i) >= 65 
+                && input.charCodeAt(i) <= 70))) {
+                    error.style.display = 'block';
+                    error.textContent = `Error : Hexadecimal number does not include '${input.charAt(i)}'. Please enter only hexadecimal number or select different base for input number`;
+                    return false;
+                }
+            }
+
+        default:
+            return true;
+    }
+}
